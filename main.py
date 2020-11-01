b480e2b8b4253d4750eae39b52e77b76d5b9dba8
Justin Varghese <laptopdell390@gmail.com>
2020-11-01 06:18:14 +0000

    initial commit

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import calendar
 print(calendar.weekheader(3))
 
 print(calendar.firstweekday())
+
+print(calendar.month(2020, 11, w=3))
+
+print(calendar.monthcalendar(2020, 11))
+
+print(calendar.calendar(2020, w = 3))
